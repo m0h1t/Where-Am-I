649a791f033661c09d671fddf338e588e93ad337
Mohit Hiralal <mohith@mohit.local>
2017-11-29 10:59:24 +0530

    Initial Commit

---
+++ Where Am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Am I
+//
+//  Created by Mohit Hiralal on 29/11/17.
+//  Copyright © 2017 Mohit Hiralal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
